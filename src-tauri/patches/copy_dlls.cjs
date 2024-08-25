179dbc9fdbf924cdb5fa8ffd9278094ac0c40f1f
Sam Cao <sjtucaocao@gmail.com>
2024-08-25 10:27:12 +0800

    🦄 refactor: Revise copy dlls script for better logging

--- src-tauri/patches/copy_dlls.cjs
+++ src-tauri/patches/copy_dlls.cjs
@@ -4,11 +4,28 @@ const process = require("process");
 
 const systemRoot = process.env.SYSTEMROOT;
 
+function copy(from, to) {
+  fs.copyFileSync(from, to);
+  console.info(`Copied from "${from}" to "${to}".`);
+}
+
 try {
-  fs.copyFileSync("..\\MediaInfoLib\\Project\\MSVC2022\\x64\\Release\\MediaInfo.dll", "src-tauri\\MediaInfo.dll");
-  fs.copyFileSync(path.join(systemRoot, "System32\\msvcp140.dll"), "src-tauri\\msvcp140.dll");
-  fs.copyFileSync(path.join(systemRoot, "System32\\vcruntime140.dll"), "src-tauri\\vcruntime140.dll");
-  fs.copyFileSync(path.join(systemRoot, "System32\\vcruntime140_1.dll"), "src-tauri\\vcruntime140_1.dll");
+  copy(
+    "..\\MediaInfoLib\\Project\\MSVC2022\\x64\\Release\\MediaInfo.dll",
+    "src-tauri\\MediaInfo.dll"
+  );
+  copy(
+    path.join(systemRoot, "System32\\msvcp140.dll"),
+    "src-tauri\\msvcp140.dll"
+  );
+  copy(
+    path.join(systemRoot, "System32\\vcruntime140.dll"),
+    "src-tauri\\vcruntime140.dll"
+  );
+  copy(
+    path.join(systemRoot, "System32\\vcruntime140_1.dll"),
+    "src-tauri\\vcruntime140_1.dll"
+  );
 } catch (error) {
   console.error(error);
   process.exit(1);
